3940f0844ca6951cc8213c8236d11175dccfdc3a
Luke Kucalaba <lkucalaba@path-robotics.com>
2022-03-22 11:18:32 -0400

    Implemented lsgpio. Started cypgioset and updated cmdline parsing. Added tools for get/set gpio line values

--- modules/cyusb-tools/src/cygpio-hammer.cpp
+++ modules/cyusb-tools/src/cygpio-hammer.cpp
@@ -1,6 +1,16 @@
 #include <CyUSBSerial/CyController.hpp>
+#include <CyUSBSerial/CyUSBSerial.hpp>
 #include <iostream>
+#include <cstring>
 #include <getopt.h>
+
+//#define ENABLE_DEBUGLOG
+
+#ifdef ENABLE_DEBUGLOG
+#define DEBUGLOG(x) std::cout << x
+#else
+#define DEBUGLOG(x)
+#endif
 
 enum class ExitCodeEnum : int
 {
@@ -10,16 +20,34 @@ enum class ExitCodeEnum : int
   UNKNOWN_CMDLINE_OPTION,
   OPTION_ARG_PARSE_ERROR,
   MISSING_OPTION_ARG,
-  CMDLINE_PARSE_ERROR
+  CMDLINE_PARSE_ERROR,
+  PRINT_USAGE_AND_EXIT
 };
 
 struct AppSettings
 {
+  /*
   bool a{};
   bool X{};
   int c{ 1 };
   int d{ 1234 };
+  */
+  std::string str_named_device;
 };
+
+static void print_usage()
+{
+  std::cout << "Usage: cygpio-hammer [options]...\n"
+               "Hammer CyUSB GPIO lines, 0->1->0->1...\n"
+               "  -n <name>    Hammer CyUSB GPIOs on a named device (must be stated)\n"
+               "  -o <n>       Offset[s] to hammer, at least one, several can be stated\n"
+               " [-c <n>]      Do <n> loops (optional, infinite loop if not stated)\n"
+               "  -h, --help   This helptext\n"
+               "\n"
+               "Example:\n"
+               "cygpio-hammer -n gpiochip0 -o 4\n"
+            << std::endl;
+}
 
 static ExitCodeEnum parse_arg_as_integer(char argname, const char* p_argvalue, int& r_value_out)
 {
@@ -42,11 +70,11 @@ static ExitCodeEnum parse_cmdline_args(int argc, char* argv[], AppSettings& r_ap
 {
   ExitCodeEnum enum_exit_code = ExitCodeEnum::SUCCESS;
 
-  static constexpr char a_short_options[] = "-:aXc:d:";
+  static constexpr char a_short_options[] = "-:hn:o:c:";  //"-:aXc:d:";
 
   int option_index = 0;  // O
   static struct option a_long_options[] = {
-    { "help", no_argument, nullptr, 0 },
+    { "help", no_argument, nullptr, 'h' },
     { nullptr, 0, nullptr, 0 }
   };
 
@@ -57,38 +85,70 @@ static ExitCodeEnum parse_cmdline_args(int argc, char* argv[], AppSettings& r_ap
     switch (opt)
     {
       case 0:  // long option
-        std::cout << "[ INFO] Option " << a_long_options[option_index].name << " was provided";
-        if(::optarg != nullptr)
+      {
+        const char* p_cstr_long_argument_name = a_long_options[option_index].name;
+        DEBUGLOG("[DEBUG] Option " << p_cstr_long_argument_name << " was provided");
+        if (::optarg == nullptr)
         {
-          std::cout << " with argument " << ::optarg;
+          DEBUGLOG(std::endl);
+          /*
+          if (std::strcmp(p_cstr_long_argument_name, "help") == 0)
+          {
+            enum_exit_code = ExitCodeEnum::PRINT_USAGE_AND_EXIT;
+          }
+          else
+          */
+          {
+            std::cout << "[ERROR] Argument " << p_cstr_long_argument_name << " unrecognized--Aborting!" << std::endl;
+            enum_exit_code = ExitCodeEnum::CMDLINE_PARSE_ERROR;
+          }
         }
-        std::cout << std::endl;
-        break;
+        else  // if(::optarg != nullptr)
+        {
+          DEBUGLOG(" with argument " << ::optarg << std::endl);
+          std::cout << "[ERROR] Error parsing command-line argument \"--" << p_cstr_long_argument_name
+                    << " with argument " << ::optarg << "\"--Aborting!" << std::endl;
+          enum_exit_code = ExitCodeEnum::CMDLINE_PARSE_ERROR;
+        }
+      }
+      break;
 
       case 1:  // regular non-option argument
         std::cout << "[ERROR] Argument " << ::optarg << " unrecognized--Aborting!" << std::endl;
         enum_exit_code = ExitCodeEnum::UNKNOWN_CMDLINE_ARG;
         break;
 
-      case 'a':
-        std::cout << "[ INFO] Option a was provided" << std::endl;
-        r_app_settings_out.a = true;
+      case 'h':
+        DEBUGLOG("[DEBUG] Option h was provided" << std::endl);
+        enum_exit_code = ExitCodeEnum::PRINT_USAGE_AND_EXIT;
         break;
 
-      case 'X':
-        std::cout << "[ INFO] Option X was provided" << std::endl;
-        r_app_settings_out.X = true;
+      case 'n':
+        DEBUGLOG("[DEBUG] Option n was provided with argument " << ::optarg << std::endl);
+        r_app_settings_out.str_named_device = ::optarg;
         break;
 
-      case 'c':
-        std::cout << "[ INFO] Option c has argument " << ::optarg << std::endl;
-        enum_exit_code = ::parse_arg_as_integer(opt, ::optarg, r_app_settings_out.c);
-        break;
+        /*
+        case 'a':
+          DEBUGLOG("[DEBUG] Option a was provided" << std::endl);
+          //r_app_settings_out.a = true;
+          break;
 
-      case 'd':
-        std::cout << "[ INFO] Option d has argument " << ::optarg << std::endl;
-        enum_exit_code = ::parse_arg_as_integer(opt, ::optarg, r_app_settings_out.d);
-        break;
+        case 'X':
+          DEBUGLOG("[DEBUG] Option X was provided" << std::endl);
+          //r_app_settings_out.X = true;
+          break;
+
+        case 'c':
+          DEBUGLOG("[DEBUG] Option c has argument " << ::optarg << std::endl);
+          //enum_exit_code = ::parse_arg_as_integer(opt, ::optarg, r_app_settings_out.c);
+          break;
+
+        case 'd':
+          DEBUGLOG("[DEBUG] Option d has argument " << ::optarg << std::endl);
+          //enum_exit_code = ::parse_arg_as_integer(opt, ::optarg, r_app_settings_out.d);
+          break;
+        */
 
       case '?':
         if (std::isprint(::optopt))
@@ -122,7 +182,6 @@ static ExitCodeEnum parse_cmdline_args(int argc, char* argv[], AppSettings& r_ap
 
     if (enum_exit_code != ExitCodeEnum::SUCCESS)
     {
-      std::cout << "[ERROR] Command-line argument parse error--Aborting!" << std::endl;
       return enum_exit_code;
     }
 
@@ -132,40 +191,42 @@ static ExitCodeEnum parse_cmdline_args(int argc, char* argv[], AppSettings& r_ap
   return enum_exit_code;
 }
 
-static void print_usage()
-{
-  std::cout << "Usage: cygpio-hammer [options]...\n"
-               "Hammer CyUSB GPIO lines, 0->1->0->1...\n"
-               "  -n <name>    Hammer CyUSB GPIOs on a named device (must be stated)\n"
-               "  -o <n>       Offset[s] to hammer, at least one, several can be stated\n"
-               " [-c <n>]      Do <n> loops (optional, infinite loop if not stated)\n"
-               "  -?, --help   This helptext\n"
-               "\n"
-               "Example:\n"
-               "cygpio-hammer -n gpiochip0 -o 4\n"
-            << std::endl;
-}
-
 int main(int argc, char* argv[])
 {
-  AppSettings r_app_settings;  // O
+  AppSettings app_settings;  // O
 
   {
-    ExitCodeEnum exit_code = ::parse_cmdline_args(argc, argv, r_app_settings);
-    if (exit_code != ExitCodeEnum::SUCCESS)
+    ExitCodeEnum enum_exit_code = ::parse_cmdline_args(argc, argv, app_settings);
+
+    if (enum_exit_code != ExitCodeEnum::SUCCESS)
     {
-      return static_cast<int>(exit_code);
+      switch (enum_exit_code)
+      {
+        case ExitCodeEnum::PRINT_USAGE_AND_EXIT:
+          enum_exit_code = ExitCodeEnum::SUCCESS;  // report success to caller
+          break;
+        default:
+          std::cout << "[ERROR] Command-line argument parse error--Aborting!" << std::endl;
+          break;
+      }
+
+      ::print_usage();
+
+      return static_cast<int>(enum_exit_code);
     }
   }
 
   cyusb::CyController cyusb_controller;
 
-  std::cout << "[ INFO] Command-line options: {"
-            << (r_app_settings.a ? " a," : " !a,")
-            << (r_app_settings.X ? " X," : " !X,")
-            << " c=" << r_app_settings.c
-            << ", d=" << r_app_settings.d
-            << " }" << std::endl;
+  DEBUGLOG("[DEBUG] Command-line options: {"
+           /*
+           << (r_app_settings.a ? " a," : " !a,")
+           << (r_app_settings.X ? " X," : " !X,")
+           << " c=" << r_app_settings.c
+           << ", d=" << r_app_settings.d
+           */
+           << " str_named_device=\"" << app_settings.str_named_device << "\""
+           << " }" << std::endl);
 
   if (!cyusb_controller.initialize())
   {
@@ -178,6 +239,43 @@ int main(int argc, char* argv[])
     return static_cast<int>(ExitCodeEnum::FAILED_TO_INITIALIZE);
   }
 
+  {
+    int gpiochip_count{};
+
+    uint8_t device_count;  // O
+    CY_RETURN_STATUS status = ::CyGetListofDevices(&device_count);
+    if (status == CY_SUCCESS)
+    {
+      for (uint8_t i = 0; i < device_count; ++i)
+      {
+        // TODO: Get device info directly from CyUSB API
+
+        CY_DEVICE_INFO device_info;  // O
+        status = ::CyGetDeviceInfo(i, &device_info);
+
+        if (status == CY_SUCCESS)
+        {
+          for (uint8_t n = 0; n < device_info.numInterfaces; ++n)
+          {
+            if (device_info.deviceType[n] == CY_TYPE_I2C)
+            {
+              const char* p_cstr_serial_number = reinterpret_cast<const char*>(device_info.serialNum);
+
+              if (app_settings.str_named_device.empty() || (app_settings.str_named_device == p_cstr_serial_number))
+              {
+                std::cout << "GPIO chip: gpiochip" << gpiochip_count << ", \""
+                          << p_cstr_serial_number << "\", ? GPIO lines" << std::endl;
+              }
+
+              ++gpiochip_count;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /*
   const std::string str_serial = "RTF001";
   cyusb_controller.set_working_device_by_serial(str_serial);
 
@@ -187,6 +285,7 @@ int main(int argc, char* argv[])
   cyusb_controller.switch_gpio_state(5);
   std::cout << "::::::::::::::::::::::::GPIO Value for GPIO 5 ::::::::::::::::::::::::::::::::" << std::endl;
   std::cout << cyusb_controller.get_gpio_value(5) << std::endl;
+  */
 
   return static_cast<int>(ExitCodeEnum::SUCCESS);
 }
--- modules/cyusb-tools/src/lscygpio.cpp
+++ modules/cyusb-tools/src/lscygpio.cpp
@@ -1,7 +1,16 @@
 #include <CyUSBSerial/CyController.hpp>
 #include <CyUSBSerial/CyUSBSerial.hpp>
 #include <iostream>
+#include <cstring>
 #include <getopt.h>
+
+//#define ENABLE_DEBUGLOG
+
+#ifdef ENABLE_DEBUGLOG
+#define DEBUGLOG(x) std::cout << x
+#else
+#define DEBUGLOG(x)
+#endif
 
 enum class ExitCodeEnum : int
 {
@@ -11,16 +20,23 @@ enum class ExitCodeEnum : int
   UNKNOWN_CMDLINE_OPTION,
   OPTION_ARG_PARSE_ERROR,
   MISSING_OPTION_ARG,
-  CMDLINE_PARSE_ERROR
+  CMDLINE_PARSE_ERROR,
+  PRINT_USAGE_AND_EXIT
 };
 
 struct AppSettings
 {
-  bool a{};
-  bool X{};
-  int c{ 1 };
-  int d{ 1234 };
+  std::string str_named_device;
 };
+
+static void print_usage()
+{
+  std::cout << "Usage: lscygpio [options]...\n"
+               "List CyUSB GPIO chips, lines and states\n"
+               "  -n <name>    List GPIOs on a named device; for example, rtf202202001\n"
+               "  -h, --help   This helptext"
+            << std::endl;
+}
 
 static ExitCodeEnum parse_arg_as_integer(char argname, const char* p_argvalue, int& r_value_out)
 {
@@ -43,11 +59,11 @@ static ExitCodeEnum parse_cmdline_args(int argc, char* argv[], AppSettings& r_ap
 {
   ExitCodeEnum enum_exit_code = ExitCodeEnum::SUCCESS;
 
-  static constexpr char a_short_options[] = "-:aXc:d:";
+  static constexpr char a_short_options[] = "-:hn:";  //"-:aXc:d:";
 
   int option_index = 0;  // O
   static struct option a_long_options[] = {
-    { "help", no_argument, nullptr, 0 },
+    { "help", no_argument, nullptr, 'h' },
     { nullptr, 0, nullptr, 0 }
   };
 
@@ -58,38 +74,70 @@ static ExitCodeEnum parse_cmdline_args(int argc, char* argv[], AppSettings& r_ap
     switch (opt)
     {
       case 0:  // long option
-        std::cout << "[ INFO] Option " << a_long_options[option_index].name << " was provided";
-        if(::optarg != nullptr)
+      {
+        const char* p_cstr_long_argument_name = a_long_options[option_index].name;
+        DEBUGLOG("[DEBUG] Option " << p_cstr_long_argument_name << " was provided");
+        if (::optarg == nullptr)
         {
-          std::cout << " with argument " << ::optarg;
+          DEBUGLOG(std::endl);
+          /*
+          if (std::strcmp(p_cstr_long_argument_name, "help") == 0)
+          {
+            enum_exit_code = ExitCodeEnum::PRINT_USAGE_AND_EXIT;
+          }
+          else
+          */
+          {
+            std::cout << "[ERROR] Argument " << p_cstr_long_argument_name << " unrecognized--Aborting!" << std::endl;
+            enum_exit_code = ExitCodeEnum::CMDLINE_PARSE_ERROR;
+          }
         }
-        std::cout << std::endl;
-        break;
+        else  // if(::optarg != nullptr)
+        {
+          DEBUGLOG(" with argument " << ::optarg << std::endl);
+          std::cout << "[ERROR] Error parsing command-line argument \"--" << p_cstr_long_argument_name
+                    << " with argument " << ::optarg << "\"--Aborting!" << std::endl;
+          enum_exit_code = ExitCodeEnum::CMDLINE_PARSE_ERROR;
+        }
+      }
+      break;
 
-      case 1:
+      case 1:  // regular non-option argument
         std::cout << "[ERROR] Argument " << ::optarg << " unrecognized--Aborting!" << std::endl;
         enum_exit_code = ExitCodeEnum::UNKNOWN_CMDLINE_ARG;
         break;
 
-      case 'a':
-        std::cout << "[ INFO] Option a was provided" << std::endl;
-        r_app_settings_out.a = true;
+      case 'h':
+        DEBUGLOG("[DEBUG] Option h was provided" << std::endl);
+        enum_exit_code = ExitCodeEnum::PRINT_USAGE_AND_EXIT;
         break;
 
-      case 'X':
-        std::cout << "[ INFO] Option X was provided" << std::endl;
-        r_app_settings_out.X = true;
+      case 'n':
+        DEBUGLOG("[DEBUG] Option n was provided with argument " << ::optarg << std::endl);
+        r_app_settings_out.str_named_device = ::optarg;
         break;
 
-      case 'c':
-        std::cout << "[ INFO] Option c has argument " << ::optarg << std::endl;
-        enum_exit_code = ::parse_arg_as_integer(opt, ::optarg, r_app_settings_out.c);
-        break;
+        /*
+        case 'a':
+          DEBUGLOG("[DEBUG] Option a was provided" << std::endl);
+          //r_app_settings_out.a = true;
+          break;
 
-      case 'd':
-        std::cout << "[ INFO] Option d has argument " << ::optarg << std::endl;
-        enum_exit_code = ::parse_arg_as_integer(opt, ::optarg, r_app_settings_out.d);
-        break;
+        case 'X':
+          DEBUGLOG("[DEBUG] Option X was provided" << std::endl);
+          //r_app_settings_out.X = true;
+          break;
+
+        case 'c':
+          DEBUGLOG("[DEBUG] Option c has argument " << ::optarg << std::endl);
+          //enum_exit_code = ::parse_arg_as_integer(opt, ::optarg, r_app_settings_out.c);
+          break;
+
+        case 'd':
+          DEBUGLOG("[DEBUG] Option d has argument " << ::optarg << std::endl);
+          //enum_exit_code = ::parse_arg_as_integer(opt, ::optarg, r_app_settings_out.d);
+          break;
+        */
 
       case '?':
         if (std::isprint(::optopt))
@@ -123,45 +171,51 @@ static ExitCodeEnum parse_cmdline_args(int argc, char* argv[], AppSettings& r_ap
 
     if (enum_exit_code != ExitCodeEnum::SUCCESS)
     {
-      std::cout << "[ERROR] Command-line argument parse error--Aborting!" << std::endl;
       return enum_exit_code;
     }
 
-        opt = ::getopt_long(argc, argv, a_short_options, a_long_options, &option_index);
+    opt = ::getopt_long(argc, argv, a_short_options, a_long_options, &option_index);
   }
 
   return enum_exit_code;
 }
 
-static void print_usage()
-{
-  std::cout << "Usage: lscygpio [options]...\n"
-               "List CyUSB GPIO chips, lines and states\n"
-               "  -n <name>    List GPIOs on a named device\n"
-               "  -?, --help   This helptext"
-            << std::endl;
-}
-
 int main(int argc, char* argv[])
 {
-  AppSettings r_app_settings;  // O
+  AppSettings app_settings;  // O
 
   {
-    ExitCodeEnum exit_code = ::parse_cmdline_args(argc, argv, r_app_settings);
-    if (exit_code != ExitCodeEnum::SUCCESS)
+    ExitCodeEnum enum_exit_code = ::parse_cmdline_args(argc, argv, app_settings);
+
+    if (enum_exit_code != ExitCodeEnum::SUCCESS)
     {
-      return static_cast<int>(exit_code);
+      switch (enum_exit_code)
+      {
+        case ExitCodeEnum::PRINT_USAGE_AND_EXIT:
+          enum_exit_code = ExitCodeEnum::SUCCESS;  // report success to caller
+          break;
+        default:
+          std::cout << "[ERROR] Command-line argument parse error--Aborting!" << std::endl;
+          break;
+      }
+
+      ::print_usage();
+
+      return static_cast<int>(enum_exit_code);
     }
   }
 
   cyusb::CyController cyusb_controller;
 
-  std::cout << "[ INFO] Command-line options: {"
-            << (r_app_settings.a ? " a," : " !a,")
-            << (r_app_settings.X ? " X," : " !X,")
-            << " c=" << r_app_settings.c
-            << ", d=" << r_app_settings.d
-            << " }" << std::endl;
+  DEBUGLOG("[DEBUG] Command-line options: {"
+           /*
+           << (r_app_settings.a ? " a," : " !a,")
+           << (r_app_settings.X ? " X," : " !X,")
+           << " c=" << r_app_settings.c
+           << ", d=" << r_app_settings.d
+           */
+           << " str_named_device=\"" << app_settings.str_named_device << "\""
+           << " }" << std::endl);
 
   if (!cyusb_controller.initialize())
   {
@@ -179,7 +233,7 @@ int main(int argc, char* argv[])
 
     uint8_t device_count;  // O
     CY_RETURN_STATUS status = ::CyGetListofDevices(&device_count);
-    if(status == CY_SUCCESS)
+    if (status == CY_SUCCESS)
     {
       for (uint8_t i = 0; i < device_count; ++i)
       {
@@ -188,28 +242,21 @@ int main(int argc, char* argv[])
         CY_DEVICE_INFO device_info;  // O
         status = ::CyGetDeviceInfo(i, &device_info);
 
-        if(status == CY_SUCCESS)
+        if (status == CY_SUCCESS)
         {
-          for(uint8_t n = 0; n < device_info.numInterfaces; ++ n)
+          for (uint8_t n = 0; n < device_info.numInterfaces; ++n)
           {
-            if(device_info.deviceType[n] == CY_TYPE_I2C)
+            if (device_info.deviceType[n] == CY_TYPE_I2C)
             {
               const char* p_cstr_serial_number = reinterpret_cast<const char*>(device_info.serialNum);
 
-              std::cout << "GPIO chip: gpiochip" << gpiochip_count << ", \""
-                        << p_cstr_serial_number << "\", ? GPIO lines" << std::endl;
+              if (app_settings.str_named_device.empty() || (app_settings.str_named_device == p_cstr_serial_number))
+              {
+                std::cout << "GPIO chip: gpiochip" << gpiochip_count << ", \""
+                          << p_cstr_serial_number << "\", ? GPIO lines" << std::endl;
+              }
+
               ++gpiochip_count;
-
-              /*
-              cyusb_controller.set_working_device_by_serial(p_cstr_serial_number);
-
-              std::cout << "::::::::::::::::::::::::GPIO Value for GPIO 5 ::::::::::::::::::::::::::::::::" << std::endl;
-              std::cout << cyusb_controller.get_gpio_value(5) << std::endl;
-
-              cyusb_controller.switch_gpio_state(5);
-              std::cout << "::::::::::::::::::::::::GPIO Value for GPIO 5 ::::::::::::::::::::::::::::::::" << std::endl;
-              std::cout << cyusb_controller.get_gpio_value(5) << std::endl;
-              */
             }
           }
         }
